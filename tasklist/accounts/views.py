57b698c4a6744cac6fdd0830af2db779c49bd0cb
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-17 06:20:27 -0300

    feat(accounts/views.py): create and deal with signup form

--- tasklist/accounts/views.py
+++ tasklist/accounts/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login, authenticate
+
+from .models import User
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            authenticate(
+                username=user.username,
+                password=user.password
+            )
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/')
+
+    else:
+        form = SignupForm()
+
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def login(request):
     return render(request, 'accounts/login.html')
