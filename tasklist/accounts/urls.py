55c16ec5a02dbfe98e61fee701b0063b4057a5e0
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-19 05:59:18 -0300

    feat(accounts/urls.py): Add logout urlpattern

--- tasklist/accounts/urls.py
+++ tasklist/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
@@ -9,5 +9,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='accounts/login.html', form_class=LoginForm), name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
