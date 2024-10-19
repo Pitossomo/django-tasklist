574bfe74c1f3ea5555bf8bff2e12869dcc4358ed
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-19 19:15:20 -0300

    feat(link/views.py): Define  create_category endpoint

--- tasklist/link/views.py
+++ tasklist/link/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import CategoryForm
+
+@login_required
+def create_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.created_by = request.user
+            category.save()
+
+            return redirect('/')
+    else:
+        form = CategoryForm()
+
+    return render(request, 'link/create_category.html', {'form': form})
