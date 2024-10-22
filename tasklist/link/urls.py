e14c976aed9828aecf258836c556e6684cec7145
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-22 16:51:49 -0300

    feat(link/urls.py): Add links to urlpatterns

--- tasklist/link/urls.py
+++ tasklist/link/urls.py
@@ -7,4 +7,5 @@ app_name = 'links'
 urlpatterns = [
     path('create-category/', views.create_category, name='create_category'),
     path('create-link/', views.create_link, name='create_link'),
+    path('links/', views.links, name='links'),
 ]
