e4974415f0528cec29257aa30af5fdb7ec3ee9d3
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-23 17:55:33 -0300

    feat(link/urls.py): Add categories view to urlpatterns

--- tasklist/link/urls.py
+++ tasklist/link/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create-category/', views.create_category, name='create_category'),
     path('create-link/', views.create_link, name='create_link'),
     path('links/', views.links, name='links'),
+    path('categories/', views.categories, name='categories'),
 ]
