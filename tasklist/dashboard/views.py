287d1562fb5ae96f22f682c830cc40ecd5456f0a
Pitossomo <pedro.h.teles@hotmail.com>
2024-10-23 05:34:02 -0300

    feat(dashboard/views.py): Add dashboard views with newest links filtered by user

--- tasklist/dashboard/views.py
+++ tasklist/dashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from link.models import Link
+
+SHOW_LAST_LINKS = 5
+
+@login_required
+def dashboard(request):
+
+    new_links = Link.objects.filter(created_by=request.user)[0:SHOW_LAST_LINKS]
+
+    return render(request, 'dashboard/dashboard.html', {'new_links': new_links})
